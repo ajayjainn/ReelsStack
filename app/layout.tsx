8201cea6d8fb88391ed91923428098c217ff1271
ajay <ajayjain1024@gmail.com>
2025-05-24 14:31:12 +0530

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+import Provider from "@/providers/provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/providers/theme-provider";
+import Header from "@/components/header";
+import { Toaster } from "@/components/ui/toaster";
+import Footer from "@/components/footer";
+
+const geist = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist-sans',
+});
+
+export const metadata: Metadata = {
+  title: "ReelsStack - AI-Generated Videos with a Click",
+  description: "ReelsStack allows users to create AI-generated videos, reels, and shorts instantly with just one click. Unlock creativity with speed and innovation.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geist.variable} antialiased mx-auto max-w-7xl`}
+          suppressHydrationWarning
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+          >
+            <Provider>
+              <Header />
+              {children}
+              <Footer />
+            </Provider>
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
